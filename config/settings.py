292c70117f9743e41d9b6fcc35db15e161249a7a
ManuIshan <manueshanchekkali@gmail.com>
2026-02-22 19:40:11 +0530

    final production settings

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
 
-DEBUG = False  # IMPORTANT for production
+DEBUG = True  # IMPORTANT for production
 
 ALLOWED_HOSTS = [
     "speako-backend-rgj1.onrender.com",
